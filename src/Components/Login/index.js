6a926f817226f619f0fc79f2b7c3c2473b5fc2f3
healfeco <healfeco@gmail.com>
2021-04-12 14:00:14 -0600

    Fixed icons align on the login page

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -10,17 +10,17 @@ export default function Login() {
                 <div className="col-12 col-lg-6 text-center">
                     <img src={logo} alt="logo" className="" />
                 </div>
-                <div className="col-12 col-lg-6 pt-4 d-flex justify-content-center text-center">
+                <div className="col-12 col-lg-6 pt-4 d-flex justify-content-center">
                     <form>
                         <div className="input-group">
                             <i className="atleta rounded-left"><img src={atleta} /></i>
                             <input class="form-control" type="text" name="username" id="username" placeholder="Username" />
                         </div>
                         <div className="input-group mt-4">
-                            <i class="fas fa-fingerprint rounded-left"></i>
+                            <i class="fas fa-fingerprint rounded-left atleta"></i>
                             <input class="form-control" type="password" name="password" id="password" placeholder="Password" />
                         </div>
-                        <button type="submit" class="btn btn-lg btn-outline-success btn-block mt-3">Log In</button>
+                        <button type="submit" class="btn btn-outline-success btn-block mt-3">Log In</button>
                     </form>
                 </div>
             </div>
