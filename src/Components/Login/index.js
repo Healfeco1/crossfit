8c27f3ca21e4490660d23851c9586b053af6947c
healfeco <healfeco@gmail.com>
2021-04-12 13:51:42 -0600

    Fixed the viewports in to login component

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -5,30 +5,25 @@ import './login.css'
 
 export default function Login() {
     return (
-        <div className="d-flex">
-            <div className="col-md-8 row justify-content-center align-items-center minh-100" id="img">
-                <img src={logo} alt="logo" className="" />
-            </div>
-            <div className="col-md-4 d-flex text-center align-items-center">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <form>
-                            {/* UserName */}
-                            <div className="input-group">
-                                <i className="atleta rounded-left"><img src={atleta} /></i>
-                                <input class="form-control" type="text" name="username" id="username" placeholder="Username" />
-                            </div>
-
-                            {/* Password */}
-                            <div className="input-group mt-5">
-                                <i class="fas fa-fingerprint rounded-left"></i>
-                                <input class="form-control" type="password" name="password" id="password" placeholder="Password" />
-                            </div>
-                                <input type="submit" value="Submit" className="btn btn-outline-success mt-3" />
-                            </form>
+        <div className="container">
+            <div className="row justify-content-center align-items-center minh-100">
+                <div className="col-12 col-lg-6 text-center">
+                    <img src={logo} alt="logo" className="" />
+                </div>
+                <div className="col-12 col-lg-6 pt-4 d-flex justify-content-center text-center">
+                    <form>
+                        <div className="input-group">
+                            <i className="atleta rounded-left"><img src={atleta} /></i>
+                            <input class="form-control" type="text" name="username" id="username" placeholder="Username" />
                         </div>
-                    </div>
+                        <div className="input-group mt-4">
+                            <i class="fas fa-fingerprint rounded-left"></i>
+                            <input class="form-control" type="password" name="password" id="password" placeholder="Password" />
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-outline-success btn-block mt-3">Log In</button>
+                    </form>
                 </div>
             </div>
+        </div>
     )
 }
