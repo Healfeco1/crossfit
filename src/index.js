9f0e03056c65556a652ced4b0ca8513d348faee5
healfeco <healfeco@gmail.com>
2021-04-27 15:23:28 -0600

    Added AuthProvider to show App component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Menu from './Components/LateralPanel/lateralPanel'
 import App from './App';
 import Login from './Components/Login/index';
+import AuthProvider from './provider/AuthProvider'
+import Dashboard from './pages/Dashboard'
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <Login /> */}
-    <App/>
-    {/* <Menu/> */}
-  </React.StrictMode>,
+  <Router>
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
+  </Router>
+  ,
   document.getElementById('root')
 );
