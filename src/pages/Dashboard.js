ece5c977a13cd7708d9d67e3d8d4b5b487eb3f14
healfeco <healfeco@gmail.com>
2021-04-27 15:25:19 -0600

    Created the Dashboard Test component

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Dashboard( {content} ) {
+    return (
+        // <div style={content}>
+        <div >
+        {/* <MenuContext.Consumer> */}
+            {/* {(contex) => sidebar} */}
+            <h1>Welcome</h1>
+        {/* </MenuContext.Consumer> */}
+        </div>
+    )
+}
