9d105a67d0a216299138392f4418902011764cff
healfeco <healfeco@gmail.com>
2021-04-27 15:28:13 -0600

    Deleted imports I not use

--- src/Components/ResetPassword/ResetPassword.js
+++ src/Components/ResetPassword/ResetPassword.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import Login from '../Login/index'
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './ResetPassword.css'
 
-export default function ResetPassword() {
+export default function ResetPassword( { setShow }) {
     return (
         <div className="text-center ml-5">
             <label className="label label-success">Se envio el correo correctamente a healfeco@gmail.com. Sigue las instrucciones del correo</label>
             <form>
             <div className="d-flex flex-column">
                 <div className="input-group justify-content-center">
-                    <i class="text-center rounded-left text-muted">@</i>
-                    <input class="form-control rounded-right" type="mail" name="mail" id="mail" placeholder="Mail" />
+                    <i className="text-center rounded-left text-muted">@</i>
+                    <input className="form-control rounded-right" type="mail" name="mail" id="mail" placeholder="Mail" />
                 </div>
-                    <button type="submit" class="btn btn-outline-danger btn-sm btn-outline-success mt-3 mb-1 align-self-lg-center">Reiniciar Contraseña</button>
-                    <Link className="font-weight-lighter text-muted text-center" to="/atletas">Iniciar Sesion</Link>
+                    <button type="submit" className="btn btn-outline-danger btn-sm btn-outline-success mt-3 mb-1 align-self-lg-center">Reiniciar Contraseña</button>
+                    <span className="font-weight-lighter text-muted text-center" onClick={()=>setShow(true)}>Iniciar Sesion</span>
             </div>
             </form>
         </div>
